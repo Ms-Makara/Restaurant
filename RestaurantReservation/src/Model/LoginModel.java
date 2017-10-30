774dc9931768f239eea97d2fa2931f6755835ee9
Killing-Machine <992400463@qq.com>
2017-10-30 13:51:07 +0000

    Model

---
+++ RestaurantReservation/src/Model/LoginModel.java
@@ -0,0 +1,45 @@
+package Model;
+
+import java.sql.ResultSet;
+import java.util.Vector;
+
+public class LoginModel
+{
+	Data.DBHelper dh = null;
+	String username,pwd;
+	Vector userInfo;
+	ResultSet rs;
+	public LoginModel(String un, String pwd)
+	{
+		
+		this.username = un;
+		this.pwd = pwd;
+		verify();
+	}
+	
+	public int verify()
+	{
+		dh = new Data.DBHelper();
+		String sql = "select * from users where username= '"
+				+ this.username+"' and password = '"+pwd+"'";
+		
+		try
+		{
+			rs = dh.ExecuteQuery(sql);
+			userInfo = new Vector(3);
+			while(rs.next())
+			{
+				userInfo.add(rs.getInt(1));
+				userInfo.add(rs.getString(2));
+				userInfo.add(rs.getInt(4));
+			}
+		} catch (Exception e)
+		{
+			// TODO: handle exception
+		}
+		
+		return (int)userInfo.get(2);
+	}
+	
+	
+}
